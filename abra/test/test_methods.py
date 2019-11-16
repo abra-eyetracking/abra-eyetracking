dcc48d549f80e76976f11160cd45b8fefc9373c2
amansourian <mansourianarman@gmail.com>
2019-11-16 15:54:34 -0800

    init testing funcs

--- abra/test/test_methods.py
+++ abra/test/test_methods.py
@@ -1,1 +1,14 @@
 # method unit testing
+import abra
+from time import sleep
+
+
+def test_method1():
+    sleep(.1)
+    assert True
+
+
+def test_method2():
+    sleep(.1)
+    assert True
+
--- abra/test/test_objects.py
+++ abra/test/test_objects.py
@@ -1,1 +1,14 @@
 # object unit testing
+import abra
+from time import sleep
+
+
+def test_object1():
+    sleep(.1)
+    assert True
+
+
+def test_object2():
+    sleep(.1)
+    assert True
+
