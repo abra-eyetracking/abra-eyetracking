6db08a4e589ae84932dd78c94eb8d4f61bf2133b
AkimaCon <conne147@mail.chapman.edu>
2019-11-24 10:49:41 -0800

    test function for data object

--- abra/test/test_objects.py
+++ abra/test/test_objects.py
@@ -3,8 +3,15 @@ import abra
 from time import sleep
 
 
-def test_object1():
-    sleep(.1)
+def test_data_object1():
+    data_test = Data()
+    assert data_test.pupil_size == [], "should be empty (1 * n_t) "  # (1 * n_t)
+    assert data_test.movement == [][], "should be empty (1 * n_t) "  # (2 * n_t)
+    assert data_test.timestamps == [], "should be empty (1 * n_t) "  # (1 * n_t)
+    assert data_test.sample_rate == 0, "should be empty int"         # (int)
+    assert data_test.messages == {}, " should be empty dict"         # (dict)
+    assert data_test.events == {}, " should be empty dict"           # (dict)
+    assert data_test.calibration == {}, "should be empty dict"       # (dict)
     assert True
 
 
