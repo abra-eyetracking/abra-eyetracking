fdc18423e61dfd8ebdc5b65141fdccb074e59ad0
Dehua Liang <andylikescodes@gmail.com>
2019-11-24 14:37:58 -0800

    Find the starting timestamps of trials, thought about the implementations of assholes

--- abra/data.py
+++ abra/data.py
@@ -1,7 +1,47 @@
 import numpy as np
+from time import sleep
+
+
+def is_number(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
 
 def read(filename):
     # TODO parse info
+    if not filename.endswith('.asc'):
+        # Raise an error for checking file name extension
+        pass
+
+
+    with open(filename) as f:
+        start_time = ''
+        flag = False
+        events = []
+        for num, line in enumerate(f, 1):
+            elements = line.split()
+            if line.startswith('START'):
+                # Find the starting time asshole
+                start_time = elements[1]
+
+            if start_time:
+                if line.startswith(start_time):
+                    flag = True
+
+            if flag == True:
+                if not is_number(elements[0]):
+                    events.append(elements[0])
+
+
+        print(np.unique(events))
+        input()
+
+
+
+
+
     return Data()
 
 class Data:
