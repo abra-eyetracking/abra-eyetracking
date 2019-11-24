5b365da366b39e03a833aa6bb139f092faae475e
AkimaCon <conne147@mail.chapman.edu>
2019-11-24 13:20:07 -0800

    Removed string after assert

--- abra/test/test_objects.py
+++ abra/test/test_objects.py
@@ -7,13 +7,13 @@ from time import sleep
 
 def test_data_object():
     data_test = Data()
-    assert data_test.pupil_size == [], "should be empty (1 * n_t) "  # (1 * n_t)
-    assert data_test.movement == [][], "should be empty (2 * n_t) "  # (2 * n_t)
-    assert data_test.timestamps == [], "should be empty (1 * n_t) "  # (1 * n_t)
-    assert data_test.sample_rate == 0, "should be empty int"         # (int)
-    assert data_test.messages == {}, " should be empty dict"         # (dict)
-    assert data_test.events == {}, " should be empty dict"           # (dict)
-    assert data_test.calibration == {}, "should be empty dict"       # (dict)
+    assert data_test.pupil_size.size == 0
+    assert data_test.movement.size == 0
+    assert data_test.timestamps.size == 0
+    assert data_test.sample_rate == 0
+    assert not data_test.messages == True
+    assert not data_test.events == True
+    assert not data_test.calibration == True
 
 def test_data_object_array_dimensions():
     data_test = Data()
