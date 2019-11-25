047a78762e712a3afb4eb0ee63fdb4ce527b7612
amansourian <mansourianarman@gmail.com>
2019-11-24 17:13:42 -0800

    updated init of Data class

--- abra/data.py
+++ abra/data.py
@@ -1,21 +1,22 @@
 import numpy as np
 
+
 def read(filename):
     # TODO parse info
     return Data()
 
+
 class Data:
 
-    def __init__(self):
-        # TODO implement initialization
-        #   pupil_size (1 * n_t)
-        #   movement (2 * n_t)
-        #   timestamps (1 * n_t)
-        #   sample_rate (int)
-        #   messages (dict)
-        #   events (dict)
-        #   calibration (dict)
-        pass
+    def __init__(self, timestamps, pupil_size, movement, sample_rate,
+                 calibration, messages, events):
+        self.timestamps = timestamps
+        self.pupil_size = pupil_size
+        self.movement = movement
+        self.sample_rate = sample_rate
+        self.calibration = calibration
+        self.messages = messages
+        self.events = events
 
     def trial_start():
         pass
