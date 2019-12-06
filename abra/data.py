92a3dcf694a590167208879d71ebe08e0ade2cc7
AkimaCon <conne147@mail.chapman.edu>
2019-12-05 17:49:48 -0800

    Gets both end and regular messages and adds them to the message dictionary

--- abra/data.py
+++ abra/data.py
@@ -19,13 +19,21 @@ def read(filename):
 
     with open(filename) as f:
         start_time = ''
+        end_time = ''
         flag = False
         events = []
+        messages_dict = {}
         for num, line in enumerate(f, 1):
             elements = line.split()
             if line.startswith('START'):
                 # Find the starting time asshole
                 start_time = elements[1]
+                end_time = ''
+            #only get END messages
+            if line.startswith('END'):
+                end_time = elements[1]
+                messages_dict[elements[1]] = elements[2:]
+                flag = False
 
             if start_time:
                 if line.startswith(start_time):
@@ -34,7 +42,19 @@ def read(filename):
             if flag == True:
                 if not is_number(elements[0]):
                     events.append(elements[0])
-
+            #finds all the END messages that is outputed
+            if end_time:
+                if line.startswith('MSG'):
+                    if elements[1] in messages_dict:
+                        messages_dict[elements[1]].append(elements[2:])
+                    else:
+                        messages_dict[elements[1]] = elements[2:]
+            #gets all MSG with time stamp as key
+            if line.startswith('MSG'):
+                if elements[1] in messages_dict:
+                    messages_dict[elements[1]].append(elements[2:])
+                else:
+                    messages_dict[elements[1]] = elements[2:]
 
         print(np.unique(events))
         input()
