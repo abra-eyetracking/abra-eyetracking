9b2046205d7d55623a5b79f53b43114a9ba85c8d
AkimaCon <conne147@mail.chapman.edu>
2019-12-08 13:00:48 -0800

    default added

--- abra/data.py
+++ abra/data.py
@@ -10,7 +10,7 @@ def is_number(s):
         return False
 
 
-def read(filename):
+def read(filename, mode = "default"):
     # TODO parse info
     if not filename.endswith('.asc'):
         # Raise an error for checking file name extension
@@ -25,61 +25,53 @@ def read(filename):
         events_dict = {}
         timestamps_list = []
         messages_dict = {}
-        for num, line in enumerate(f, 1):
-            elements = line.split()
-            if line.startswith('START'):
-                # Find the starting time asshole
-                start_time = elements[1]
-                end_time = ''   #to end "END message input"
-                timestamps_list.append(elements[1])
-            #to only get END messages
-            if line.startswith('END'):
-                end_time = elements[1]
-                messages_dict[elements[1]] = elements[2:]
-                timestamps_list.append(elements[1])
-                flag = False
-
-            if start_time:
-                if line.startswith(start_time):
-                    flag = True
-
-            if flag == True:
-                if elements[0] == "MSG":
-
+        trial_markers = {"start":[], "end":[]}
+        if mode == "default":
+            for num, line in enumerate(f, 1):
+                elements = line.split()
+                if line.startswith('START'):
+                    # Find the starting time asshole
+                    start_time = elements[1]
+                    trial_markers["start"].append(elements[1])
+                    end_time = ''   #to end "END message input"
                     timestamps_list.append(elements[1])
-                elif not is_number(elements[0]):
-                    events.append(elements[0])
-                    if str(elements[0] + " " + elements[1]) not in events_dict:
-                        events_dict[str(elements[0] + " " + elements[1])] = elements[2:]
-                        timestamps_list.append(elements[2])
-                    else:
-                        events_dict[str(elements[0] + " " + elements[1])].append(elements[2:])
-                        timestamps_list.append(elements[2])
-                    # print(events_dict)
-                    # input()
-                else:
-                    timestamps_list.append(elements[0])
-                print(timestamps_list)
-                input()
-
-            #finds all the END messages that is outputed
-            if end_time:
-                if line.startswith('MSG'):
-                    if elements[1] in messages_dict:
-                        messages_dict[elements[1]].append(elements[2:])
-                        #print(messages_dict[elements[1]])
-                        #input()
-                    else:
-                        messages_dict[elements[1]] = elements[2:]
-                        #print(messages_dict)
-                        #input()
-            #gets all MSG with time stamp as key
-            if line.startswith('MSG'):
-                if elements[1] in messages_dict:
-                    messages_dict[elements[1]].append(elements[2:])
-                else:
+                #to only get END messages
+                if line.startswith('END'):
+                    end_time = elements[1]
+                    trial_markers["end"].append(elements[1])
                     messages_dict[elements[1]] = elements[2:]
+                    timestamps_list.append(elements[1])
+                    flag = False
+                    start_time = ''
 
+                if start_time:
+                    if line.startswith(start_time):
+                        flag = True
+                # check for start
+                if flag == True:
+                    #gets messages between default start and end
+                    if elements[0] == "MSG":
+                        timestamps_list.append(elements[1])
+                        messages_dict[elements[1]] = elements[2:]
+                    elif not is_number(elements[1]):
+                        events.append(elements[0])
+                        if str(elements[0] + " " + elements[1]) not in events_dict:
+                            events_dict[str(elements[0] + " " + elements[1])] = elements[2:]
+                            timestamps_list.append(elements[2])
+                        else:
+                            events_dict[str(elements[0] + " " + elements[1])].append(elements[2:])
+                            timestamps_list.append(elements[2])
+
+                    else:
+                        timestamps_list.append(elements[0])
+
+                #finds all the END messages that is outputed
+                if end_time:
+                    if line.startswith('MSG'):
+                        if elements[1] in messages_dict:
+                            messages_dict[elements[1]].append(elements[2:])
+                        else:
+                            messages_dict[elements[1]] = elements[2:]
         print(np.unique(events))
         input()
 
