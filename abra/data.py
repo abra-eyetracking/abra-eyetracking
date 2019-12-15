333804ca18ad111b5d684c63e30b1306138090ed
AkimaCon <conne147@mail.chapman.edu>
2019-12-14 16:02:20 -0800

    default updated to return data object

--- abra/data.py
+++ abra/data.py
@@ -26,6 +26,9 @@ def read(filename, mode = "default"):
         timestamps_list = []
         messages_dict = {}
         trial_markers = {"start":[], "end":[]}
+        pupil_size_list = []
+        movement_list = [[],[]]
+
         if mode == "default":
             for num, line in enumerate(f, 1):
                 elements = line.split()
@@ -35,7 +38,9 @@ def read(filename, mode = "default"):
                     trial_markers["start"].append(elements[1])
                     end_time = ''   #to end "END message input"
                     timestamps_list.append(elements[1])
+
                 #to only get END messages
+                #adds to trial_markers, messages_dict, timestamps_list
                 if line.startswith('END'):
                     end_time = elements[1]
                     trial_markers["end"].append(elements[1])
@@ -50,20 +55,26 @@ def read(filename, mode = "default"):
                 # check for start
                 if flag == True:
                     #gets messages between default start and end
-                    if elements[0] == "MSG":
+                    # will get pupil size, timestamps, and movements
+                    if is_number(elements[0]):
+                        timestamps_list.append(elements[0])
+                        pupil_size_list.append(elements[1])
+                        movement_list[0].append(elements[2]) # x-axis
+                        movement_list[1].append(elements[3]) # y-axis
+
+                    elif elements[0] == "MSG":
                         timestamps_list.append(elements[1])
                         messages_dict[elements[1]] = elements[2:]
+
                     elif not is_number(elements[1]):
                         events.append(elements[0])
+                        timestamps_list.append(elements[2])
                         if str(elements[0] + " " + elements[1]) not in events_dict:
                             events_dict[str(elements[0] + " " + elements[1])] = elements[2:]
-                            timestamps_list.append(elements[2])
+                            # timestamps_list.append(elements[2])
                         else:
                             events_dict[str(elements[0] + " " + elements[1])].append(elements[2:])
-                            timestamps_list.append(elements[2])
-
-                    else:
-                        timestamps_list.append(elements[0])
+                            # timestamps_list.append(elements[2])
 
                 #finds all the END messages that is outputed
                 if end_time:
@@ -72,14 +83,14 @@ def read(filename, mode = "default"):
                             messages_dict[elements[1]].append(elements[2:])
                         else:
                             messages_dict[elements[1]] = elements[2:]
-        print(np.unique(events))
-        input()
+    #remove duplicates
+    timestamps_list= list(dict.fromkeys(timestamps_list))
+    #convert list to numpy array
+    timestamps = np.array(timestamps_list)
+    pupil_size = np.array(pupil_size_list)
+    movement = np.array(movement_list)
 
-
-
-
-
-    return Data()
+    return Data(timestamps, pupil_size, movement, 500, {}, messages_dict, events_dict)
 
 
 class Data:
