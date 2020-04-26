fa908639dc3e4314132c9cfe7ebc9ae96b8bb3e6
AkimaCon <conne147@mail.chapman.edu>
2020-04-25 23:57:08 -0700

    updated layout and out-of-index functions

--- abra/Visualization.py
+++ abra/Visualization.py
@@ -67,34 +67,47 @@ class Pupil_Vis(tk.Frame):
 
         # self.next_graph
 
-        good_button = ttk.Button(self, text="Good",
+        good_button = ttk.Button(self,
+                            text="Good",
                             command=lambda: self.good_button_function())
-        good_button.pack()
+        good_button.pack(side = tk.TOP)
 
-        bad_button = ttk.Button(self, text="Bad",
+        bad_button = ttk.Button(self,
+                            text="Bad",
                             command=lambda: self.bad_button_function())
-        bad_button.pack()
+        bad_button.pack(side = tk.TOP)
 
-        next_button = ttk.Button(self, text="Next",
+        next_button = ttk.Button(self,
+                            text="Next",
                             command=lambda: self.next_graph())
-        next_button.pack()
+        next_button.pack(side = tk.TOP)
 
-        previous_button = ttk.Button(self, text="Previous",
+        previous_button = ttk.Button(self,
+                            text="Previous",
                             command=lambda: self.previous_graph())
-        previous_button.pack()
+        previous_button.pack(side = tk.TOP)
 
+        self.my_text = ttk.Label(self,
+         text= f"Graph: {self.index +1}     Quality: {self.quality_list[self.index]}")
+        self.my_text.pack(side = tk.TOP)
 
         self.f = Figure(figsize=(5,5), dpi=100)
         self.a = self.f.add_subplot(111)
-        self.a.plot(self.timestamp_trials[index],self.pupil_trials[index])
+        self.a.plot(self.timestamp_trials[index],
+                    self.pupil_trials[index])
 
         self.canvas = FigureCanvasTkAgg(self.f, self)
         self.canvas.draw()
-        self.canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-
         self.toolbar = NavigationToolbar2Tk(self.canvas, self)
         self.toolbar.update()
-        self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        self.canvas._tkcanvas.pack(side=tk.BOTTOM,
+                                   fill=tk.BOTH,
+                                   expand=True)
+        self.canvas.get_tk_widget().pack(side=tk.BOTTOM,
+                                         fill=tk.BOTH,
+                                         expand=True)
+
+
 
     #gets next graph by getting adding 1 to the current position (index)
     def next_graph(self):
@@ -104,21 +117,33 @@ class Pupil_Vis(tk.Frame):
         self.index += 1
 
         if(self.index >= len(self.pupil_trials)):
-            messagebox.showinfo('End of List')
+            messagebox.askquestion('Finished',
+                        'This is the last graph\nDo you want to exit?')
+            if finished == 'yes':
+                    self.quit()
             self.index -= 1
             pass
         self.f = Figure(figsize=(5,5), dpi=100)
         self.a = self.f.add_subplot(111)
-        self.a.plot(self.timestamp_trials[index],self.pupil_trials[index])
+        self.a.plot(self.timestamp_trials[index],
+                    self.pupil_trials[index])
 
         self.canvas = FigureCanvasTkAgg(self.f, self)
         self.canvas.draw()
-        self.canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+        self.canvas.get_tk_widget().pack(side=tk.BOTTOM,
+                                         fill=tk.BOTH,
+                                         expand=True)
 
         # self.toolbar = NavigationToolbar2Tk(self.canvas, self)
         self.toolbar.update()
-        self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        self.canvas._tkcanvas.pack(side=tk.BOTTOM,
+                                   fill=tk.BOTH,
+                                   expand=True)
 
+        self.my_text.pack_forget()
+        self.my_text = ttk.Label(self,
+                                text= f"Graph: {self.index +1}     {self.quality_list[self.index]}")
+        self.my_text.pack(side = tk.TOP)
     #gets previous graph by subtracting 1 to the current position (index)
     def previous_graph(self):
         # get pupil size data from index
@@ -126,28 +151,49 @@ class Pupil_Vis(tk.Frame):
         self.canvas.get_tk_widget().destroy()
         self.index -= 1
         if(self.index < 0):
-            messagebox.showinfo('Out of Index','You are at the begining of the List')
+            messagebox.showinfo('Out of Index',
+                            'You are at the begining of the List')
             self.index = 0
             pass
         self.f = Figure(figsize=(5,5), dpi=100)
         self.a = self.f.add_subplot(111)
-        self.a.plot(self.timestamp_trials[index],self.pupil_trials[index])
+        self.a.plot(self.timestamp_trials[index],
+                    self.pupil_trials[index])
 
         self.canvas = FigureCanvasTkAgg(self.f, self)
         self.canvas.draw()
-        self.canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+        self.canvas.get_tk_widget().pack(side=tk.BOTTOM,
+                                         fill=tk.BOTH,
+                                         expand=True)
 
         # self.toolbar = NavigationToolbar2Tk(self.canvas, self)
         self.toolbar.update()
-        self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        self.canvas._tkcanvas.pack(side=tk.BOTTOM,
+                                   fill=tk.BOTH,
+                                   expand=True)
+
+        self.my_text.pack_forget()
+        self.my_text = ttk.Label(self,
+                                 text= f"Graph: {self.index +1}     {self.quality_list[self.index]}")
+        self.my_text.pack(side = tk.TOP)
 
     #if current graph is good, change value to 1
     def good_button_function(self):
-        self.quality_list[index] = 1
+        self.quality_list[self.index] = 1
+
+        self.my_text.pack_forget()
+        self.my_text = ttk.Label(self,
+                                 text= f"Graph: {self.index +1}     {self.quality_list[self.index]}")
+        self.my_text.pack(side = tk.TOP)
 
     #if current grpah is bad, change value to 0
     def bad_button_function(self):
-        self.quality_list[index] = 0
+        self.quality_list[self.index] = 0
+
+        self.my_text.pack_forget()
+        self.my_text = ttk.Label(self,
+                                 text= f"Graph: {self.index +1}     {self.quality_list[self.index]}")
+        self.my_text.pack(side = tk.TOP)
 
 
 default_obj = data.read("abra/test/asc/1211NE1.asc")
